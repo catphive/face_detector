9d5ffa46b134c136f137e8789e994dcc1a34f2a3
Brendan Miller <catphive@catphive.net>
2012-03-06 22:25:07 -0800

    Unit tests for features C and D.

--- faces_test.py
+++ faces_test.py
@@ -149,5 +149,71 @@ class TestFeatureB(unittest.TestCase):
                        [16,17,18,19,20],[21,22,23,24,25]],
                       (1,1), (5,3), -40)
 
+class TestFeatureC(unittest.TestCase):
+    
+    def validate(self, ar, start, end, output):
+        ar = numpy.array(ar)
+        faces.integrate_image(ar)
+        img = faces.IntegratedImage(ar)
+        self.assertEqual(faces.feature_c(img, start, end), output)
+
+    def test_min(self):
+        self.validate([[0,0,0]], (0,0), (1,3), 0)
+        self.validate([[1,0,0]], (0,0), (1,3), -1)
+        self.validate([[1,0,1]], (0,0), (1,3), -2)
+        self.validate([[0,0,1]], (0,0), (1,3), -1)
+        self.validate([[0,1,0]], (0,0), (1,3), 1)
+        self.validate([[2,0,0]], (0,0), (1,3), -2)
+        self.validate([[0,0,2]], (0,0), (1,3), -2)
+        self.validate([[2,0,1]], (0,0), (1,3), -3)
+        self.validate([[1,2,0]], (0,0), (1,3), 1)
+    
+    def test_med(self):
+        self.validate([[0,0,0],[0,0,0]], (0,0), (2,3), 0)
+        self.validate([[1,0,0],[0,1,0]], (0,0), (2,3), 0)
+        self.validate([[0,0,0,0,0,0],[0,0,0,0,0,0]], (0,0), (2,6), 0)
+        self.validate([[1,0,0,0,0,0],[0,0,0,0,0,0]], (0,0), (2,6), -1)
+        self.validate([[0,0,1,0,0,0],[0,3,0,0,0,0]], (0,0), (2,6), -2)
+        self.validate([[0,0,0,1,0,0],[0,3,0,0,0,0]], (0,0), (2,6), -2)
+        self.validate([[0,0,0,1,3,0],[0,0,0,0,0,0]], (0,0), (2,6), -2)
+        self.validate([[0,1,2,0,0,0],[0,0,0,0,1,0]], (0,0), (2,6), 0)
+
+    def test_translated(self):
+        self.validate([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]], (1,1), (3,7), 0)
+        self.validate([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21]], (1,1), (2,7), -23)
+        self.validate([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21]], (1,2), (3,5), -29)
+
+class TestFeatureD(unittest.TestCase):
+    
+    def validate(self, ar, start, end, output):
+        ar = numpy.array(ar)
+        faces.integrate_image(ar)
+        img = faces.IntegratedImage(ar)
+        self.assertEqual(faces.feature_d(img, start, end), output)
+
+    def test_min(self):
+        self.validate([[0,0],[0,0]], (0,0), (2,2), 0)
+        self.validate([[1,0],[0,0]], (0,0), (2,2), -1)
+        self.validate([[0,1],[0,0]], (0,0), (2,2), 1)
+        self.validate([[1,1],[0,0]], (0,0), (2,2), 0)
+        self.validate([[0,0],[1,0]], (0,0), (2,2), 1)
+        self.validate([[1,0],[1,0]], (0,0), (2,2), 0)
+        self.validate([[0,1],[1,0]], (0,0), (2,2), 2)
+        self.validate([[1,1],[1,0]], (0,0), (2,2), 1)
+        self.validate([[0,0],[0,1]], (0,0), (2,2), -1)
+        self.validate([[1,0],[0,1]], (0,0), (2,2), -2)
+        self.validate([[0,1],[0,1]], (0,0), (2,2), 0)
+        self.validate([[1,1],[0,1]], (0,0), (2,2), -1)
+        self.validate([[0,0],[1,1]], (0,0), (2,2), 0)
+        self.validate([[1,0],[1,1]], (0,0), (2,2), -1)
+        self.validate([[0,1],[1,1]], (0,0), (2,2), 1)
+        self.validate([[1,1],[1,1]], (0,0), (2,2), 0)
+        self.validate([[4,3],[2,1]], (0,0), (2,2), 0)
+
+    def test_translated(self):
+        self.validate([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]], (1,2), (3,4), 0)
+        self.validate([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21]], (1,1), (3,3), 0)
+        self.validate([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21]], (1,2), (3,6), 0)
+
 if __name__ == '__main__':
     unittest.main()
